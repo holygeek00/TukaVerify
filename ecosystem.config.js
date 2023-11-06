e4015e4089b47bc24e6a5699ca6e5ca6b31219d3
liuqingfeng <719993988@qq.com>
2023-11-06 20:53:50 +0800

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,18 @@
 module.exports = {
-  apps: [
-    {
-      name: 'ReactApp',
-      script: './server.js',
-      exec_mode: 'cluster',
-      instances: 'max', // This will use all the CPU cores for load balancing
-      env: {
-        NODE_ENV: 'production', // Make sure to set the environment variable to 'production'
-
-      }
-
-    }
-  ]
+  apps: [{
+    name: 'ReactApp',
+    script: 'npm',
+    args: 'start',
+    watch: true,
+    env: {
+      NODE_ENV: 'development',
+      PORT: 3000 // default port for create-react-app
+    },
+    env_production: {
+      NODE_ENV: 'production',
+      PORT: 5000 // you can specify a different port for production
+    },
+    // Setting max_memory_restart is optional to restart your app if it exceeds the specified memory limit.
+    max_memory_restart: '500M' // Restart the app if it reaches 500MB memory usage
+  }]
 }
